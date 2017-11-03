54b0e1322ea916c88c23aec36794efa5fa6e8c35
Cameron Chaparro <cccons@tuta.io>
2017-11-02 23:12:57 -0500

    Update the script
    Actually call the backup function in our script
    
    Get rid of superfluous echo statements in the backup function
    
    Handle arguments to rsync by building them up, if needed
    
    Add some more command-line flags
    
    Get rid of default values

--- backup.sh
+++ backup.sh
@@ -5,40 +5,45 @@ version="1.0.0"
 
 backup_files_and_dirs=()
 backup_to_dir=${backup_to_dir:-}
-include_from_file=${include_from_file:-$HOME/includes.txt}
-exclude_from_file=${exclude_from_file:-$HOME/excludes.txt}
+files_from_file=${files_from_file:-}
+include_from_file=${include_from_file:-}
+exclude_from_file=${exclude_from_file:-}
+
+quiet=
 
 backup() {
-    echo "Starting backup..."
-    if [ -f $include_from_file -a -d $backup_to_dir ]; then
-        echo "  backing up files in      : ${include_from_file}"
-        if [ -f $exclude_from_file ]; then
-            echo "  excluding patterns from   : ${exclude_from_file}"
-        else
-            echo "  excluding these patterns : ${excluded_patterns}"
-        fi
-        echo
-        echo "  backing files up from : ${HOME}"
-        echo "  backing files up to   : ${backup_directory}"
-        echo
+    if [ -z "$quiet" ]; then
+        echo "running backup command:"
+        echo "  rsync `build_rsync_args_list`"
         read -p "continue? (y/n) " continue
-        if [ "$continue" == "y" ]; then
-            if [ -f $exclude_from_file ]; then
-                rsync --files-from=$include_from_file --exclude-from=$exclude_from_file --delete -avzzr $HOME $backup_to_dir
-            else
-                rsync --files-from=$include_from_file --exclude=$excluded_patterns --delete -avzzr $HOME $backup_to_dir
-            fi
-            return_code=$?
-            echo "done"
-        else
-            echo "aborting!"
-            return_code=1
-        fi
-        exit $return_code
-    else
-        echo "error: '$backup_to_dir' is not available"
-        exit -1
+        [ ! $(expr "$continue" : "[yY]$") -ne 0 ] && echo "aborting!" && exit 0
     fi
+    rsync `build_rsync_args_list`
+}
+
+build_rsync_args_list() {
+    rsync_args=()
+    [ ! -z "$files_from_file" ] && rsync_args+=(--files-from="$files_from_file")
+    [ ! -z "$include_from_file" ] && rsync_args+=(--include-from="$include_from_file")
+    [ ! -z "$exclude_from_file" ] && rsync_args+=(--exclude-from="$exclude_from_file")
+    [ ! -z "$quiet" ] && rsync_args+=(--quiet)
+    rsync_args+=(--delete -avzzr "${backup_files_and_dirs[@]}" "$backup_to_dir")
+    echo "${rsync_args[@]}"
+}
+
+print_backup_param_values() {
+    echo    "Printing values of backup parameters:"
+    echo -e "  backup directory  : ${backup_to_dir}"
+    echo -e "  files-from file   : ${files_from_file}"
+    echo -e "  include-from file : ${include_from_file}"
+    echo -e "  exclude-from file : ${exclude_from_file}"
+    echo
+    read -p "Display files / directories to be backed up? (y/n) " display
+    [ $(expr "$display" : "[yY]$") -ne 0 ] && \
+        for file_or_dir in "${backup_files_and_dirs[@]}"; do
+            echo "  $file_or_dir"
+        done
+    return 0
 }
 
 process_cmd_args() {
@@ -50,6 +55,11 @@ process_cmd_args() {
                 [ ! -d "$backup_to_dir" ] && cli_error "'$backup_to_dir' does not exist"
                 shift
                 ;;
+            -f|--files-from)
+                [ ! -z "${2:+defined}" ] && files_from_file="$2" || cli_error "files-from file is required"
+                [ ! -f "$files_from_file" ] && cli_error "'$files_from_file' does not exist"
+                shift
+                ;;
             -i|--include-from)
                 [ ! -z "${2:+defined}" ] && include_from_file="$2" || cli_error "include-from file is required"
                 [ ! -f "$include_from_file" ] && cli_error "'$include_from_file' does not exist"
@@ -59,6 +69,10 @@ process_cmd_args() {
                 [ ! -z "${2:+defined}" ] && exclude_from_file="$2" || cli_error "exclude-from file is required"
                 [ ! -f "$exclude_from_file" ] && cli_error "'$exclude_from_file' does not exist"
                 shift
+                ;;
+            -q|--quiet)
+                print_values=
+                quiet=yes
                 ;;
             --print)
                 print_values=yes
@@ -82,6 +96,7 @@ process_cmd_args() {
 
     [ ! -z "$print_values" ] && print_backup_param_values && exit 0
     [ -z "$backup_dir_set" ] && cli_error "-b or --backup-dir flag is required"
+    [ "${#backup_files_and_dirs[@]}" -eq 0 ] && cli_error "no file(s) / directory(ies) to backup"
 }
 
 cli_error() {
@@ -101,10 +116,10 @@ note: required parameters to long options are required for short options, too
 
 options:
   -b,--backup-dir DIR       The directory in which to backup the files (required)
+  -f,--files-from FILE      The file containing files to include in the backup
   -i,--include-from FILE    The file containing patterns to include in the backup
-                               (default: $include_from_file)
   -e,--exclude-from FILE    The file containing patterns to exclude from the backup
-                               (default: $exclude_from_file)
+  -q,--quiet                Only display output if there are errors
      --print                Print backup parameter values without performing a backup
 
   -h,--help                 Display this help message and exit
@@ -116,17 +131,5 @@ version() {
     echo "$script_name v $version"
 }
 
-print_backup_param_values() {
-    echo    "Printing values of backup parameters:"
-    echo -e "  backup directory  : ${backup_to_dir}"
-    echo -e "  include-from file : ${include_from_file}"
-    echo -e "  exclude-from file : ${exclude_from_file}"
-    echo
-    read -p "Display files / directories to be backed up? (y/n) " display
-    [ "$display" = "y" -o "$display" = "Y" ] && \
-        for file_or_dir in "${backup_files_and_dirs[@]}"; do
-            echo "  $file_or_dir"
-        done
-}
-
 process_cmd_args "$@"
+backup
