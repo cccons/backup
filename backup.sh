4c33fb48872226754cea112e0f42872f7e047456
Cameron Chaparro <cccons@tuta.io>
2017-11-03 18:40:55 -0500

    Add a --dry-run flag
    Add a --dry-run flag and associated functionality
    
    Add a short description to the usage message
    
    Convert the '! -z' checks to '-n' checks (since they check for the same thing
    anyways

--- backup.sh
+++ backup.sh
@@ -9,24 +9,26 @@ files_from_file=${files_from_file:-}
 include_from_file=${include_from_file:-}
 exclude_from_file=${exclude_from_file:-}
 
+dry_run=
 quiet=
 
 backup() {
-    if [ -z "$quiet" ]; then
+    if [ -z "$quiet" -o -n "$dry_run" ]; then
+        echo
         echo "running backup command:"
         echo "  rsync `build_rsync_args_list`"
         read -p "continue? (y/n) " continue
         [ ! $(expr "$continue" : "[yY]$") -ne 0 ] && echo "aborting!" && exit 0
     fi
-    rsync `build_rsync_args_list`
+    [ -z "$dry_run" ] && rsync `build_rsync_args_list`
 }
 
 build_rsync_args_list() {
     rsync_args=()
-    [ ! -z "$files_from_file" ] && rsync_args+=(--files-from="$files_from_file")
-    [ ! -z "$include_from_file" ] && rsync_args+=(--include-from="$include_from_file")
-    [ ! -z "$exclude_from_file" ] && rsync_args+=(--exclude-from="$exclude_from_file")
-    [ ! -z "$quiet" ] && rsync_args+=(--quiet)
+    [ -n "$files_from_file" ] && rsync_args+=(--files-from="$files_from_file")
+    [ -n "$include_from_file" ] && rsync_args+=(--include-from="$include_from_file")
+    [ -n "$exclude_from_file" ] && rsync_args+=(--exclude-from="$exclude_from_file")
+    [ -n "$quiet" ] && rsync_args+=(--quiet)
     rsync_args+=(--delete -avzzr "${backup_files_and_dirs[@]}" "$backup_to_dir")
     echo "${rsync_args[@]}"
 }
@@ -47,28 +49,32 @@ print_backup_param_values() {
 }
 
 process_cmd_args() {
-    while [ ! -z "${1:+defined}" ]; do
+    while [ -n "${1:+defined}" ]; do
         case "$1" in
             -b|--backup-dir)
                 backup_dir_set=yes
-                [ ! -z "${2:+defined}" ] && backup_to_dir="$2" || cli_error "backup directory is required"
+                [ -n "${2:+defined}" ] && backup_to_dir="$2" || cli_error "backup directory is required"
                 [ ! -d "$backup_to_dir" ] && cli_error "'$backup_to_dir' does not exist"
                 shift
                 ;;
             -f|--files-from)
-                [ ! -z "${2:+defined}" ] && files_from_file="$2" || cli_error "files-from file is required"
+                [ -n "${2:+defined}" ] && files_from_file="$2" || cli_error "files-from file is required"
                 [ ! -f "$files_from_file" ] && cli_error "'$files_from_file' does not exist"
                 shift
                 ;;
             -i|--include-from)
-                [ ! -z "${2:+defined}" ] && include_from_file="$2" || cli_error "include-from file is required"
+                [ -n "${2:+defined}" ] && include_from_file="$2" || cli_error "include-from file is required"
                 [ ! -f "$include_from_file" ] && cli_error "'$include_from_file' does not exist"
                 shift
                 ;;
             -e|--exclude-from)
-                [ ! -z "${2:+defined}" ] && exclude_from_file="$2" || cli_error "exclude-from file is required"
+                [ -n "${2:+defined}" ] && exclude_from_file="$2" || cli_error "exclude-from file is required"
                 [ ! -f "$exclude_from_file" ] && cli_error "'$exclude_from_file' does not exist"
                 shift
+                ;;
+            -n|--dry-run)
+                print_values=yes
+                dry_run=yes
                 ;;
             -q|--quiet)
                 print_values=
@@ -94,7 +100,7 @@ process_cmd_args() {
         shift
     done
 
-    [ ! -z "$print_values" ] && print_backup_param_values && exit 0
+    [ -n "$print_values" ] && print_backup_param_values && [ -z "$dry_run" ] && exit 0
     [ -z "$backup_dir_set" ] && cli_error "-b or --backup-dir flag is required"
     [ "${#backup_files_and_dirs[@]}" -eq 0 ] && cli_error "no file(s) / directory(ies) to backup"
 }
@@ -108,6 +114,8 @@ cli_error() {
 usage() {
     version
     cat <<EOF
+Perform a backup of the specified files and / or directories
+
 usage: $script_name -h | --help
        $script_name -v | --version
        $script_name [options...] <directory... | file...>
@@ -119,8 +127,9 @@ options:
   -f,--files-from FILE      The file containing files to include in the backup
   -i,--include-from FILE    The file containing patterns to include in the backup
   -e,--exclude-from FILE    The file containing patterns to exclude from the backup
+  -n,--dry-run              Perform a dry run (show the output without performing a backup)
   -q,--quiet                Only display output if there are errors
-     --print                Print backup parameter values without performing a backup
+     --print                Print backup parameter values
 
   -h,--help                 Display this help message and exit
   -v,--version              Display version information and exit
